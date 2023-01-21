a050e890602ee5f33399577c38bc52239790d172
Omid Deldar <omiddeldar1380.od@gmail.com>
2023-01-21 15:30:50 +0330

    add monitoring websocket to xrp

--- src/xrp-ledger/xrp.module.ts
+++ src/xrp-ledger/xrp.module.ts
@@ -2,12 +2,13 @@ import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { XrpController } from "./controller/xrp.controller";
 import { XrpWalletEntity } from "./entities/xrp.entity";
+import { MonitoringXrpService } from "./service/monitoring.service";
 import { XrpService } from "./service/xrp.service";
 
 
 @Module({
     imports:[TypeOrmModule.forFeature([XrpWalletEntity])],
-    providers:[XrpService],
+    providers:[XrpService, MonitoringXrpService],
     controllers:[XrpController]
 })
 
